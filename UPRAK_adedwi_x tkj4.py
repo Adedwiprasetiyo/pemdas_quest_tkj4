e46ae625c81be75d79918c2b779b7d75e4801ba4
Adedwiprasetiyo <112526392+Adedwiprasetiyo@users.noreply.github.com>
2022-09-09 14:52:22 +0700

    Add files via upload

---
+++ UPRAK_adedwi_x tkj4.py
@@ -0,0 +1,41 @@
+# rumus volume balok panjang * lebar * tinggi
+
+
+panjang = 10
+lebar = 15
+tinggi = 10
+volume_b = panjang * lebar * tinggi
+print("volume balok adalah",volume_b)
+
+
+# rumus volume kubus sisi * sisi * sisi
+
+sisi = 15
+sisi = 20
+sisi = 15
+volume_k = sisi * sisi * sisi
+print("volume kubus adalah",volume_k)
+
+
+# rumus limas 1/3 * luas alas * tinggi
+
+a = 10
+t = 20
+luas_l = a * t /3
+print("rumus luas permukaan limas adalah", volume_l)
+
+
+# rumus luas tabung 22/7 x r x r x t
+
+r = 8
+r = 8
+t = 10
+luas_t = r * r * t /7
+print("rumus luas tabung adalah",luas_t)
+
+# celcius to reamur celcius * 4/5
+
+celcius = 20
+reamur = 15
+suhu = 20 * 4/5
+print("celcius dijadikan reamur",suhu)
